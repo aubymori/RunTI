0a1348d90d2e0602520024001a1999ac51def05e
aubymori <aubyomori@gmail.com>
2025-02-24 18:49:29 -0600

    Initial commit

---
+++ resource.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define IDI_RUNDLG               100
+
+#define DLG_RUN                  200
+#define IDD_ICON                 201
+#define IDD_PROMPT               202
+#define IDD_RUNDLGOPENPROMPT     203
+#define IDD_COMMAND              204
+#define IDD_BROWSE               205
---
+++ runti.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <windows.h>
+#include "resource.h"
+#include <tlhelp32.h>
+#include "wil/resource.h"
